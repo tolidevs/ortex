35a789fd89f9f0ca641fcc8ae07653576c1afa93
Tolani Benson <tolani.benson@tandem.co.uk>
2021-01-30 15:57:57 +0000

    added material-ui

---
+++ ortex-login/src/theme/theme.ts
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+        main: '#D3CFC6',
+        },
+        secondary: {
+        main: '#34acac',
+        },
+        background: {
+            default: '#202020'
+        }
+    },
+});
+
+export default theme
